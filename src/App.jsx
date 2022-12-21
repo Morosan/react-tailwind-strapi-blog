fd794f910b5e26b2ec34f10c2a8d8c9d6449b69a
Andrei M <98398587+andrei-wm@users.noreply.github.com>
2022-12-21 14:51:28 +0200

    WIP strapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import { Homepage, BlogContentPage } from "./pages";
+import {Routes, Route} from 'react-router-dom';
+import useFetch from './hooks/useFetch'
+
+
+export default function App() {
+  let {loading, data, error} =useFetch('http://localhost:1337/api/blogs?populate=*')
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error!</p>
+
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<Homepage blogs={data?data:""} />}></Route>
+        <Route path='/blog/:id' element={<BlogContentPage  blogs={data?data:""}/>}></Route>
+      </Routes>
+    </div>
+  
   )
 }
-
-export default App
